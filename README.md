e842407f8f9837d60dd6925b791636780e3f9c22
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-05-27 07:55:27 +0200

    dark mode refacto

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Portfolio
 
+Online at : https://alexandrecoin.github.io/portfolio/
 Simple online HTML/CSS resume. Nothing more, nothing less, and nothing fancy.
+
--- index.js
+++ index.js
@@ -42,8 +42,6 @@ function init() {
   renderBackground();
 }
 
-// Content rendering
-
 function renderJobs() {
   var experienceUl = document.getElementById('experience');
   for (let i = 0; i < experience.length; i++) {
@@ -61,35 +59,5 @@ function renderBackground() {
     backgroundUl.appendChild(newLi);
   }
 }
-
-// Dark mode
-
-var toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]',
-);
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  }
-}
-
-var currentTheme = localStorage.getItem('theme')
-  ? localStorage.getItem('theme')
-  : null;
-
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-
-  if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-  }
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
 
 window.onload = init();
