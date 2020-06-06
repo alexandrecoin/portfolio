d621cbd4012b288a1e86754c31a14812f821e611
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-06-06 06:51:21 +0200

    Added html/css card

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var experience = [
   {
     title: 'Junior R&D Engineer',
     companyName: 'Cubyn',
-    startDate: '',
-    endDate: '',
-    description: '',
+    startDate: '2019-11',
+    endDate: '2020-02',
+    description: 'omg',
   },
   {
     title: 'JS/TS Full-Stack Developer',
@@ -46,7 +46,14 @@ function renderJobs() {
   var experienceUl = document.getElementById('experience');
   for (let i = 0; i < experience.length; i++) {
     var newLi = document.createElement('li');
-    newLi.appendChild(document.createTextNode(experience[i].title));
+    let expTemplate1 = `
+      ${experience[i].title} • ${experience[i].companyName} • ${experience[i].startDate} - ${
+        experience[i].endDate
+      }
+    ${experience[i].description}
+    `;
+    newLi.appendChild(document.createTextNode(expTemplate1));
+    newLi.style.marginBottom = '0.5em';
     experienceUl.appendChild(newLi);
     experienceUl.style.listStyleType = 'none';
   }
@@ -57,6 +64,7 @@ function renderBackground() {
   for (let i = 0; i < background.length; i++) {
     var newLi = document.createElement('li');
     newLi.appendChild(document.createTextNode(background[i].diploma));
+    newLi.style.marginBottom = '0.5em';
     backgroundUl.appendChild(newLi);
     backgroundUl.style.listStyleType = 'none';
   }
